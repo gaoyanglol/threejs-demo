9c95792a9cfd7db21945bd54866ff8dff06a8571
gaoyanglol <gaoyanglol@hotmail.com>
2019-12-25 16:34:50 +0800

    load models

--- js/train.js
+++ js/train.js
@@ -1,7 +1,12 @@
+// import * as THREE from './vendor/three/three.js';
+// import * as GLTFLoader from "./vendor/three/GLTFLoader";
+
 let container;
 let scene;
 let camera;
 let renderer;
+const mixers = [];
+const clock = new THREE.Clock();
 
 function init() {
   container = document.querySelector('#scene-container');
@@ -11,10 +16,11 @@ function init() {
   createCamera();
   createControls();
   createLights();
-  createMeshes();
+  loadModels();
   createRenderer();
 
   renderer.setAnimationLoop( () => {
+    update();
     render();
   });
 }
@@ -23,10 +29,10 @@ function createCamera() {
   camera = new THREE.PerspectiveCamera(
     35,
     container.clientWidth / container.clientHeight,
-    0.1,
+    1,
     100
   );
-  camera.position.set(-5, 5, 7);
+  camera.position.set(-1.5, 1.5, 6.5);
 }
 
 function createControls() {
@@ -45,79 +51,39 @@ function createLights() {
   scene.add(ambientLight, mainLight);
 }
 
-function createMaterials() {
-  const body = new THREE.MeshStandardMaterial({
-    color: 0xff3333,
-    flatShading: true,
-  });
+function loadModels() {
+  const loader = new THREE.GLTFLoader();
 
-  body.color.convertSRGBToLinear();
+  const onLoad = (gltf, position) => {
+    const model = gltf.scene.children[0];
+    model.position.copy(position);
 
-  const detail = new THREE.MeshStandardMaterial({
-    color: 0x333333,
-    flatShading: true,
-  });
+    const animation = gltf.animations[0];
 
-  detail.color.convertSRGBToLinear();
+    const mixer = new THREE.AnimationMixer(model);
+    mixers.push(mixer);
 
-  return {
-    body,
-    detail,
-  };
-}
+    const action = mixer.clipAction(animation);
+    action.play();
 
-function createGeometrics() {
-  const nose = new THREE.CylinderBufferGeometry(0.75, 0.75, 3, 12);
-  const cabin = new THREE.BoxBufferGeometry(2, 2.25, 1.5);
-  const chimney = new THREE.CylinderBufferGeometry(0.3, 0.1, 0.5);
-  const wheel = new THREE.CylinderBufferGeometry(0.4, 0.4, 1.75, 16);
-  wheel.rotateX(Math.PI / 2);
-
-  return {
-    nose,
-    cabin,
-    chimney,
-    wheel,
+    scene.add(model);
   }
-}
 
-function createMeshes() {
-  const train = new THREE.Group();
-  scene.add(train);
+  const onProgress = () => {}
 
-  const materials = createMaterials();
-  const geometries = createGeometrics();
+  const onError = (errorMessage) => {
+    console.log(errorMessage);
+  }
 
-  const nose = new THREE.Mesh(geometries.nose, materials.body);
-  nose.rotation.z = Math.PI / 2;
-  nose.position.x = -1;
-  
-  const cabin = new THREE.Mesh(geometries.cabin, materials.body);
-  cabin.position.set(1.5, 0.4, 0);
+  const parrotPosition = new THREE.Vector3(0, 0, 2.5);
+  loader.load('../models/Parrot.glb', gltf => onLoad(gltf, parrotPosition), onProgress, onError);
 
-  const chimney = new THREE.Mesh(geometries.chimney, materials.detail);
-  chimney.position.set(-2, 0.9, 0);
+  const flamingoPosition = new THREE.Vector3(7.5, 0, -10);
+  loader.load('../models/Flamingo.glb', gltf => onLoad(gltf, flamingoPosition), onProgress, onError);
 
-  const smallWheelRear = new THREE.Mesh(geometries.wheel, materials.detail);
-  smallWheelRear.position.set(0, -0.5, 0);
-  const smallWheelCenter = smallWheelRear.clone();
-  smallWheelCenter.position.x = -1;
-  const smallWheelFront = smallWheelRear.clone();
-  smallWheelFront.position.x = -2;
+  const storkPosition = new THREE.Vector3(0, -2.5, -10);
+  loader.load('../models/Stork.glb', gltf => onLoad(gltf, storkPosition), onProgress, onError);
 
-  const bigWheel = smallWheelRear.clone();
-  bigWheel.scale.set(2, 2, 1.25);
-  bigWheel.position.set(1.5, -0.1, 0);
-
-  train.add(
-    nose,
-    cabin,
-    chimney,
-    smallWheelRear,
-    smallWheelCenter,
-    smallWheelFront,
-    bigWheel,
-  );
 }
 
 function createRenderer() {
@@ -131,6 +97,14 @@ function createRenderer() {
   renderer.physicallyCorrectLights = true;
   container.appendChild(renderer.domElement);
 }
+
+function update() {
+  const delta = clock.getDelta();
+  for (const mixer of mixers) {
+    mixer.update(delta);
+  }
+}
+
 function render() {
   renderer.render(scene, camera);
 }
